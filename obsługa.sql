3e8404e6b5a0b5e582acba01793aa6bf00aad569
blonski <225959@student.pwr.edu.pl>
2018-06-04 23:50:02 +0200

    lista klientow

--- obsługa.sql
+++ obsługa.sql
@@ -1,1 +1,4 @@
 SELECT * FROM WSZY_PRAC; -- wyświetlij wszystkich pracowników z budynkami , stnaowiskiem i numerem telefonu
+SELECT * FROM MAGAZYNY;
+SELECT * FROM DOSTAWY_MAGAZY1; -- 1-7 magazyny
+SELECT * from lista_klientow;
